faf194ff19733830e5bf3a70423800eee073c72b
Alex Kutuev <kutuev93@yandex.ru>
2025-07-17 00:17:32 +0100

    add UTs

--- tests/test_folder_tree_scanner.py
+++ tests/test_folder_tree_scanner.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from app.folder_tree_scanner import DirectoryType, get_master_folder_tree
+from app.folder_tree_scanner import DirectoryItem, DirectoryType, get_master_folder_tree
 
 def test_get_master_folder_tree(tmp_path: Path):
     structure = [
@@ -12,32 +12,60 @@ def test_get_master_folder_tree(tmp_path: Path):
         "file.txt",
     ]
 
+    create_test_structure(tmp_path, structure)
+
+    master_directory = get_master_folder_tree(tmp_path)
+    sorted_master_child_items = sorted(master_directory.child_items, key=lambda item: item.name)
+
+    assert master_directory.type == DirectoryType.FOLDER
+
+    assert sorted_master_child_items[0].name == "dir_1"
+    assert sorted_master_child_items[0].type == DirectoryType.FOLDER
+
+    assert sorted_master_child_items[0].child_items[0].name == "dir_1_1"
+    assert sorted_master_child_items[0].type == DirectoryType.FOLDER
+
+    assert sorted_master_child_items[0].child_items[0].child_items[0].name == "file.txt"
+    assert sorted_master_child_items[0].child_items[0].child_items[0].type == DirectoryType.FILE
+
+    assert sorted_master_child_items[1].name == "dir_2"
+    assert sorted_master_child_items[1].type == DirectoryType.FOLDER
+
+    assert sorted_master_child_items[1].child_items[0].name == "dir_2_1"
+    assert sorted_master_child_items[1].child_items[0].type == DirectoryType.FOLDER
+    assert sorted_master_child_items[1].child_items[1].name == "dir_2_2"
+    assert sorted_master_child_items[1].child_items[1].type == DirectoryType.FOLDER
+
+    assert sorted_master_child_items[2].name == "file.txt"
+    assert sorted_master_child_items[2].type == DirectoryType.FILE
+
+
+def test_get_master_only_files(tmp_path: Path):
+    structure = [
+        "file.txt",
+        "file_1.txt",
+        "file_2.txt",
+    ]
+
+    create_test_structure(tmp_path, structure)
+
+    master_directory = get_master_folder_tree(tmp_path)
+    sorted_master_child_items = sorted(master_directory.child_items, key=lambda item: item.name)
+
+
+    assert sorted_master_child_items[0].name == "file.txt"
+    assert sorted_master_child_items[0].type == DirectoryType.FILE
+
+    assert sorted_master_child_items[1].name == "file_1.txt"
+    assert sorted_master_child_items[1].type == DirectoryType.FILE
+
+    assert sorted_master_child_items[2].name == "file_2.txt"
+    assert sorted_master_child_items[2].type == DirectoryType.FILE
+
+def create_test_structure(tmp_path: Path, structure: list[str]):
     for item in structure:
         if item.endswith(".txt"):
             (tmp_path / item).parent.mkdir(parents=True, exist_ok=True)
             (tmp_path / item).touch()
         else:
-            (tmp_path / item).mkdir()
-            
-    masterDirectory = get_master_folder_tree(tmp_path)
-    
-    assert masterDirectory.child_items[0].name == "dir_1"
-    assert masterDirectory.child_items[0].type == DirectoryType.FOLDER
-
-    assert masterDirectory.child_items[0].child_items[0].name == "dir_1_1"
-    assert masterDirectory.child_items[0].type == DirectoryType.FOLDER
-
-    assert masterDirectory.child_items[0].child_items[0].child_items[0].name == "file.txt"
-    assert masterDirectory.child_items[0].child_items[0].child_items[0].type == DirectoryType.FILE
-
-    assert masterDirectory.child_items[1].name == "dir_2"
-    assert masterDirectory.child_items[1].type == DirectoryType.FOLDER
-
-    assert masterDirectory.child_items[1].child_items[0].name == "dir_2_1"
-    assert masterDirectory.child_items[1].child_items[0].type == DirectoryType.FOLDER
-    assert masterDirectory.child_items[1].child_items[1].name == "dir_2_2"
-    assert masterDirectory.child_items[1].child_items[1].type == DirectoryType.FOLDER
-
-    assert masterDirectory.child_items[2].name == "file.txt"
-    assert masterDirectory.child_items[2].name == DirectoryType.FILE
-
+            (tmp_path / item).mkdir(parents=True, exist_ok=True)
